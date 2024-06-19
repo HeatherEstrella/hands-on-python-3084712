f73d972356b4e5568d0adca86535dc13408992ee
Heather Estrella <98059403+HeatherEstrella@users.noreply.github.com>
2024-06-19 04:27:39 +0000

    First 2 trainings complete

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,5 +1,6 @@
 greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
+extened_grt2 = greet + "You are amazing!"
 
 name = "John"
 
@@ -10,3 +11,7 @@ greet_format = "Hello {}"
 formatted = greet_format.format(name)
 
 print(intrupution, formatted)
+
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("John", "Paul"))
